9bc8dbbf7c8ef48c5e69d0ddf25d1cad8698dab3
Sairam <Sairam@Sai>
2020-02-10 19:14:29 +0530

    Variables Added by Sai

--- src/com/nt/test/Test.java
+++ src/com/nt/test/Test.java
@@ -3,6 +3,10 @@ package com.nt.test;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("main Method");
+		int x=10;
+		int y=20;
+		System.out.println("c :"+x+y);
 		
 	}
 }
