6b428762f5542d8a0ac19b725dc0c32a23096bf6
Sairam <Sairam@Sai>
2020-02-10 20:57:11 +0530

    sopln changed by sai

--- src/com/nt/test/Test.java
+++ src/com/nt/test/Test.java
@@ -6,8 +6,8 @@ public class Test {
 		System.out.println("main Method");
 		int x=10;
 		int y=20;
-		int c=0;
-		System.out.println("c :"+x+y);
+		int c=x+y;
+		System.out.println("c :"+c);
 		
 	}
 }
